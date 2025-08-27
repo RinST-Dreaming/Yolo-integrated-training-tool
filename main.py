79dc5ae0addc9cf0994051a14a87635ec13d4bcd
RinST-Dreaming <544250926@qq.com>
2025-08-27 14:39:55 +0800

    Revert "main_menu main_menu_function main.py" revert the unexpected commit This reverts commit 094f3c1d8f82c2d841acb7ebbbbb2da5cb84e2dd.

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ class MainWindow(QtWidgets.QMainWindow):
         # 设置语言切换信号
         self.setup_language_switch()
         
-        # 加载默认翻译
-        self.load_translation(self.current_language)
-        
     def setup_language_switch(self):
         # 假设界面中有一个名为languageComboBox的组合框用于选择语言
         if hasattr(self.ui, 'languageComboBox'):
@@ -30,35 +27,19 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QApplication.removeTranslator(self.translator)
         
         # 根据选择加载新的翻译
-        if language == self.tr("中文"):
-            self.load_translation("zh_CN")
+        translation_dir = os.path.join(os.path.dirname(__file__), 'translations')
+        if language == "中文":
+            translation_file = 'zh_CN.qm'
+            self.current_language = "zh_CN"
         else:  # English
-            self.load_translation("en_US")
+            translation_file = 'en_US.qm'
+            self.current_language = "en_US"
+            
+        if self.translator.load(translation_file, translation_dir):
+            QtWidgets.QApplication.installTranslator(self.translator)
             
         # 重新翻译界面
         self.ui.retranslateUi(self)
-        
-    def load_translation(self, language_code):
-        # 获取翻译文件路径
-        translation_dir = os.path.join(os.path.dirname(__file__), 'translations')
-        translation_file = f'{language_code}.qm'
-        translation_path = os.path.join(translation_dir, translation_file)
-        
-        # 加载翻译文件
-        if os.path.exists(translation_path):
-            if self.translator.load(translation_path):
-                QtWidgets.QApplication.installTranslator(self.translator)
-                self.current_language = language_code
-            else:
-                print(self.tr("加载翻译文件失败: {file}").format(file=translation_path))
-        else:
-            print(self.tr("翻译文件不存在: {file}").format(file=translation_path))
-
-    def changeEvent(self, event):
-        # 处理语言更改事件
-        if event.type() == QtCore.QEvent.LanguageChange:
-            self.ui.retranslateUi(self)
-        super(MainWindow, self).changeEvent(event)
 
 if __name__ == "__main__":
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
